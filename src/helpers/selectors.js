3022c9dc95643319601200528599ad1ec07c45f1
Jade <jade.duong@telus.com>
2023-01-15 20:32:36 -0800

    add selectors and useVisualMode hook

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,13 +2,13 @@
 
 export function getAppointmentsForDay(state, day) {
   let results = [];
-  let dayArr= [];
+  let dayArr = [];
   for (const curDay of state.days) {
     if (day === curDay.name) {
       dayArr = curDay.appointments
     }
   }
-  
+
   for (const dayApt of dayArr) {
     for (const key of Object.keys(state.appointments)) {
       if (parseInt(key) === dayApt) {
@@ -18,4 +18,19 @@ export function getAppointmentsForDay(state, day) {
   }
   return results;
 }
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null
+  }
+  const interviewerID = interview.interviewer;
+  for (let ID in state.interviewers) {
+    if (interviewerID === parseInt(ID)) {
+      let result = { ...interview }
+      result.interviewer = state.interviewers[ID];
+      return result;
+    }
+  }
+}
+
 
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+
+const useVisualMode = function(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
+
+  const transition = function(newMode, replace = false) {
+    setMode(newMode);
+
+    if (!replace) {
+      history.push(newMode)
+    }
+    
+    setHistory([...history])
+  }
+  const back = function() {
+    if (history.length > 1) {
+      history.pop()
+      setHistory([...history])
+      setMode(history[history.length - 1])
+    }
+    
+  }
+  return {
+    mode,
+    transition,
+    back,
+    history
+  }
+}
+
+export default useVisualMode;
